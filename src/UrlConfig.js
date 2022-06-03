3c617029809db1bd4160577a183f1101ac38654f
AkshayUpadhyay2411 <75607973+AkshayUpadhyay2411@users.noreply.github.com>
2022-06-03 16:53:04 +0530

    urlconfig final4

--- src/UrlConfig.js
+++ src/UrlConfig.js
@@ -1,3 +1,3 @@
-const baseUrl = window.location.hostname === 'localhost' ? 'http://localhost:2000/api' : 'https://my-vcode-backend.herokuapp.com/'
+const baseUrl = window.location.hostname === 'localhost' ? 'http://localhost:2000/api' : 'https://my-vcode-backend.herokuapp.com'
 
 export const api = `${baseUrl}/api`;
